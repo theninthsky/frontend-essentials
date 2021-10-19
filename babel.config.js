b239124c70c36c765651c2e4df46c7453adbbe60
Almog Gabay <theninesky@gmail.com>
2021-10-19 09:34:43 +0300

    Fix es modules version

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 export default {
-  presets: ['@babel/preset-env']
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { browsers: process.env.ESM ? 'defaults and supports es6-module' : 'defaults' },
+        modules: process.env.ESM ? false : 'auto'
+      }
+    ]
+  ]
 }
--- prepublish.sh
+++ prepublish.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 rm cjs esm -rf
-BROWSERSLIST_ENV=cjs npx babel src --out-dir cjs
-BROWSERSLIST_ENV=esm npx babel src --out-dir esm
+
+npx babel src --out-dir cjs
+ESM=true npx babel src --out-dir esm
+
 cp src/hooks/*.d.ts cjs/hooks
 cp src/hooks/*.d.ts esm/hooks
