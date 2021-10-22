5c4d4b250b3618772336c1bda10d439df14e71f6
Almog Gabay <theninesky@gmail.com>
2021-10-22 15:25:52 +0300

    Add react preset to babel

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
         targets: { browsers: process.env.ESM ? 'defaults and supports es6-module' : 'defaults' },
         modules: process.env.ESM ? false : 'auto'
       }
-    ]
+    ],
+    ['@babel/preset-react', { runtime: 'automatic' }]
   ]
 }
